62f0b757dbe39ffd6707e9e7e9fb0d96722c08be
Alena Hrynkevich <alena@appveyor.com>
2019-08-13 17:24:12 -0700

    After adding build.ps1 we have 3 files in the dir

--- build.ps1
+++ build.ps1
@@ -1,4 +1,4 @@
-if((Get-ChildItem).Length -ne 2) { throw "Wrong number of files in build directory."; }
+if((Get-ChildItem).Length -ne 3) { throw "Wrong number of files in build directory."; }
 Write-Host "Checking appveyor_build_folder"
 $expected_build_folder = "C:\my-projects\dir with spaces\for test project"
 'if($env:appveyor_build_folder -ne $expected_build_folder) { throw "appveyor_build_folder: $env:appveyor_build_folder, expected: $expected_build_folder"; }'
